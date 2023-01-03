1d42a9e258f7ebc7f42a8e407e40a50dab6017c0
Jordan Guzman <jordan-guzman-salas@hotmail.com>
2023-01-02 20:57:49 -0600

    Navbar y sistema de logout

--- client/src/PlataformaVirtual/pages/menuInicial/MenuInicial.jsx
+++ client/src/PlataformaVirtual/pages/menuInicial/MenuInicial.jsx
@@ -4,12 +4,19 @@ import { useContext } from "react"
 
 import { UserContext } from "../../context/UserContext"
 
+import { NavBar } from "../../../ui/components/NavBar/NavBar";
 
 export const MenuInicial = () => {
-  const { loged } = useContext(UserContext);
+  const { loged, userData, logOutUser } = useContext(UserContext);
 
   console.log(loged);
   return (
-    <div>MenuInicial</div>
+    <div>
+      <NavBar 
+        nombreUsuario = { userData.nombre }
+        onLogOut = { logOutUser }
+      />
+      MenuInicial
+    </div>
   )
 }
---
+++ client/src/ui/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+
+
+import { Typography, AppBar, IconButton } from '@mui/material'
+import { LogoutOutlined } from '@mui/icons-material'
+
+import Logo from '../../../PaginaPrincipal/assets/Logo.svg'
+import './NavBar.css'
+
+export const NavBar = ({ nombreUsuario, onLogOut }) => {
+  return (
+    <AppBar sx={{ display: 'flex', flexDirection: 'row', backgroundColor: '#4FA4D3'}}>
+        <img className='NavBar-Logo' src={Logo} alt="Logo del colegio" />
+
+        <div className='NavBar-ButonsContainer'>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, mr: 3 }}>
+                { nombreUsuario }
+            </Typography>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={onLogOut}
+                color="inherit"
+            >
+                <LogoutOutlined />
+            </IconButton>
+        </div>
+    </AppBar>
+  )
+}
